9fc44176863288901620465cfcc4423fa0f8f41d
Nadeem Khan <khan.datawave@gmail.com>
2026-04-10 20:11:20 +0530

    api-fix

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
+const API_URL = import.meta.env.VITE_API_URL || "https://clg-project-cpvk.onrender.com";
+
 const api = axios.create({
-  baseURL: '/api',
+  baseURL: API_URL,
 });
 
 api.interceptors.request.use(
@@ -12,9 +14,7 @@ api.interceptors.request.use(
     }
     return config;
   },
-  (error) => {
-    return Promise.reject(error);
-  }
+  (error) => Promise.reject(error)
 );
 
 api.interceptors.response.use(
